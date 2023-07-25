539ff3366ec157f68d685e41cacd586e120e48e6
CanadianDoc <78990604+CanadianDoc@users.noreply.github.com>
2023-07-25 17:54:15 -0600

    leagueroll update

---
+++ src/commands/fun/leagueroll.js
@@ -0,0 +1,33 @@
+const { SlashCommandBuilder, EmbedBuilder } = require("discord.js");
+const axios = require("axios");
+const { REST } = require("@discordjs/rest");
+const { Routes } = require("discord-api-types/v10");
+
+module.exports = {
+  data: new SlashCommandBuilder()
+    .setName("leagueroll")
+    .setDescription("Let's spin the roulette!..."),
+  async execute(interaction, bot) {
+    const lane = interaction.options.getString("lane");
+    try {
+      const latestPatch = (
+        await axios.get("https://ddragon.leagueoflegends.com/api/versions.json")
+      ).data[0];
+      const allChampionsData = (
+        await axios.get(
+          `https://ddragon.leagueoflegends.com/cdn/${latestPatch}/data/en_US/champion.json`
+        )
+      ).data.data;
+      const champions = Object.entries(allChampionsData);
+
+      const randomIndex = Math.floor(Math.random() * champions.length);
+      const [championName, { tags }] = champions[randomIndex];
+      await interaction.reply(`Your champion is: ${championName}!`);
+    } catch (error) {
+      console.error(error);
+      await interaction.reply(
+        "An error occurred while processing your request."
+      );
+    }
+  },
+};
